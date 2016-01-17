2207036d0ffcc5583a46c3b8b6d320e3e2d289db
Kananek Thongkam <info.dvgamer@gmail.com>
2016-01-17 18:57:37 +0700

    remove modules

--- modules/component.js
+++
@@ -1,6 +0,0 @@
-module.exports =  [
-    { name: "HOME", component:'', ejs: 'component/main' },
-    { name: "PORTFOLIO", component:'portfolio', ejs:'component/portfolio' },
-    { name: "CONTACT", component:'contact', ejs:'component/contact' },
-];
-
