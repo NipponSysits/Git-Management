07db3ae754740c4b5f3c88e4a7ef5310cab6063e
Kananek Thongkam <info.dvgamer@gmail.com>
2016-01-24 00:43:24 +0700

    sessions 4 LEVEL

--- systems.js
+++ systems.js
@@ -18,9 +18,12 @@ var language = require('./language/'+(user.language || 'en-EN'));
 var crypto = require('crypto').createHmac('sha256', PrimaryKey.key);
 var encryptor = require('simple-encryptor')(PrimaryKey.key);
 
+var SQLError = function(err){ err = err || {}; return { name: err.name, message: (err != null ? '('+err.statusCode+') '+err.code+' - '+err.message : "")};  }
+
 var SessionClient = function(req, res, next){
 	var onTimestamp = Date.now();
-	var onExpire = 86400000 + onTimestamp;
+	var Hour24 = 86400000; // milisecond
+	var onExpire = Hour24 + onTimestamp;
 	var session = (req.headers['x-session-client'] || 'null') === 'null' ? null : req.headers['x-session-client'];
 	var requested = req.headers['x-requested'] != undefined && req.headers['x-session-client'] != undefined;
 
@@ -28,18 +31,27 @@ var SessionClient = function(req, res, next){
 	req.expire = onExpire;
 	req.XHRRequested = req.xhr && requested;
 
-	if(req.xhr && requested) { 
+	if(req.xhr && requested) { // LEVEL 1 
+		req.XHRRequested = false;
 		req.session = session;
 		var decrypted = encryptor.decrypt(session);
-
-		console.log('decrypted', /(.+?)>\+</.exec(decrypted));
-		next();
-	}
-
-	if(!session) {
-		//var hash = crypto.update(onTimestamp.toString()).digest('hex');
-		var encrypted = encryptor.encrypt(PrimaryKey.key+'>+<'+onTimestamp.toString());
-		req.session = encrypted;
+		if((/(.+?)>\+</.exec(decrypted) || [])[1] === PrimaryKey.key) { // LEVEL 2
+			var db = conn.connect();
+		  	db.select('sys_sessions', { session_id: req.session }, function(err, row, field){ //LEVEL 3
+		  		if(!err) {
+		  			q.all([ // LEVEL 4
+			  			db.insert('sys_requested', { request_id: req.headers['x-requested'], created_at: req.timestamp }),
+			  			db.update('sys_sessions', { session_id: req.session }, { created_at: req.timestamp }),
+			  			db.query('DELETE FROM sys_requested WHERE created_at <= :yesterday OR created_at >= :tomorrow', { yesterday: onTimestamp-Hour24, tomorrow:onTimestamp+Hour24 }),
+		  			]).then(function(){
+		  				req.XHRRequested = true;
+		  				next();
+		  			});
+		  		}
+			});
+		}
+	} else {
+		if(!session) req.session = encryptor.encrypt(PrimaryKey.key+'>+<'+onTimestamp.toString());
 		next();
 	}
 }
@@ -47,13 +59,13 @@ var SessionClient = function(req, res, next){
 app.api = function(path, callback){
 	console.log('API:', path);
 	app.post(path, [ SessionClient, bodyParser.json(), bodyParser.urlencoded() ], function(req, res){
+		console.log('XHRRequested', req.XHRRequested);
 		if(req.XHRRequested) {
-			res.error = function(data, title, message){
-				res.send({ onError: true, exTitle: title || "", onMessage: message || "", getItems: data })
-			}
+			res.error = function(data, title, message){ res.send({ onError: true, exTitle: title || "", onMessage: message || "", getItems: data }) }
+			res.success = function(data, title, message){ res.send({ onError: false, exTitle: title || "", onMessage: message || "", getItems: data }) }
 			callback(req, res, req.body);
 		} else {
-			res.code(404);
+			res.status(404).send('Not found');
 		}
 		//res.end();
 	});
@@ -71,8 +83,10 @@ app.get('*', [ SessionClient ], function(req, res) {
 	var db = conn.connect();
   	var $scope = {};
   	db.select('sys_sessions', { session_id: req.session }, function(err, row, field){
-  		if(row.length == 0) db.insert('sys_sessions', { session_id: req.session, email: null, expire_at: (10 * 60 * 1000) + req.timestamp });
-  		res.render('index', { _LANG: language, _HOST: config.ip+':'+config.port, _SESSION_ : req.session });
+  		if(row == null) db.insert('sys_sessions', { session_id: req.session, email: null, expire_at: 0, created_at: req.timestamp });
+  		err = err || {};
+  		var message = (err != null ? '('+err.statusCode+') '+err.code+' - '+err.message : "");
+  		res.render('index', { _LANG: language, _HOST: config.ip+':'+config.port, _SESSION_ : req.session, _EX_NAME : err.name , _EX_MESSAGE : message });
 	});
 });
 
