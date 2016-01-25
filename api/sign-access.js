52fba3f935648e8f840e1fdb2101c34c335629dd
Kananek Thongkam <info.dvgamer@gmail.com>
2016-01-26 00:54:53 +0700

    change api access

--- api/sign-access.js
+++ api/sign-access.js
@@ -2,11 +2,12 @@ var conn = require('../library/db');
 
 module.exports = function(req, res, data){
 	var db = conn.connect();
+	db.update('sys_sessions', { access_id: req.access }, { session_id: req.session });
   	db.selectOne('sys_sessions', { email: data.email }, function(err, row, field){ //LEVEL 3
   		if(row) { // Usernamne found
-			res.success({ access_again: true });
+			res.success({ access: true });
   		} else {
-  			res.succeess({ access_again: false });
+  			res.success({ access: false });
   		}
 	});
 }
--- systems.js
+++ systems.js
@@ -1,6 +1,6 @@
 var http = require('http'), engine = require('ejs-mate'), express = require('express'), app = express();
 var q = require('q'), mysql = require('mysql'), walk = require('walk');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser'), cookieParser = require('cookie-parser')
 
 var PrimaryKey = { key: 'T0UnO.K-Sentinel' };
 var conn = require('./library/db');
@@ -31,40 +31,47 @@ var SessionClient = function(req, res, next){
 	req.expire = onExpire;
 	req.XHRRequested = req.xhr && requested;
 	req.pathname = req._parsedUrl.pathname;
+	req.access = req.cookies.ACCESS || '';
 
 	if(req.xhr && requested) { // LEVEL 1 
 		req.XHRRequested = false;
 		req.session = session;
 		var decrypted = encryptor.decrypt(session);
+
+
 		if((/(.+?)>\+</.exec(decrypted) || [])[1] === PrimaryKey.key) { // LEVEL 2
 			var db = conn.connect();
 		  	db.select('sys_sessions', { session_id: req.session }, function(err, row, field){ //LEVEL 3
 		  		if(!err) {
-		  			var whereTime = { yesterday: onTimestamp-Hour24, tomorrow:onTimestamp+Hour24, today:onTimestamp };
+		  			var whereTime = { yesterday: req.timestamp-Hour24, tomorrow:req.timestamp+Hour24, today:req.timestamp };
 		  			var sqlSession = 'DELETE FROM sys_sessions ' +
 		  				'WHERE created_at <= :yesterday OR created_at >= :tomorrow OR (expire_at < :today AND expire_at > 0)';
 		  			q.all([
-			  			db.insert('sys_requested', { request_id: req.headers['x-requested'], created_at: req.timestamp }),
-			  			db.update('sys_sessions', { session_id: req.session }, { created_at: req.timestamp }),
+			  			db.insert('sys_requested', { access_id: req.access, request_id: req.headers['x-requested'], created_at: req.timestamp }),
+			  			db.update('sys_sessions', { expire_at: req.timestamp }, { session_id: req.session }),
 			  			db.query('DELETE FROM sys_requested WHERE created_at <= :yesterday OR created_at >= :tomorrow ', whereTime),  // LEVEL 4
 			  			db.query(sqlSession, whereTime)
 		  			]).then(function(){
 		  				req.XHRRequested = true;
+		  				next();
+		  			}).catch(function(ex){
+		  				console.log(ex);
+		  				req.XHRRequested = false;
 		  				next();
 		  			});
 		  		}
 			});
 		}
 	} else {
-		if(!session) req.session = encryptor.encrypt(PrimaryKey.key+'>+<'+onTimestamp.toString());
+		if(!session) req.session = encryptor.encrypt(PrimaryKey.key+'>+<'+req.timestamp.toString());
 		next();
 	}
+
 }
 
 app.api = function(path, callback){
 	console.log('API:', path);
-	app.post(path, [ SessionClient, bodyParser.json(), bodyParser.urlencoded() ], function(req, res){
-		console.log('XHRRequested', req.XHRRequested);
+	app.post(path, [ cookieParser(), SessionClient, bodyParser.json(), bodyParser.urlencoded()], function(req, res){
 		if(req.XHRRequested) {
 			res.error = function(data, title, message){ res.send({ onError: true, exTitle: title || "", onMessage: message || "", getItems: data }) }
 			res.success = function(data, title, message){ res.send({ onError: false, exTitle: title || "", onMessage: message || "", getItems: data }) }
@@ -72,7 +79,6 @@ app.api = function(path, callback){
 		} else {
 			res.status(404).send('Not found');
 		}
-		//res.end();
 	});
 }
 
@@ -84,23 +90,24 @@ walk.walk('api', { followLinks: false }).on('file', function(root, stat, next) {
     next();
 });
 
-app.get('*', [ SessionClient ], function(req, res) {
+app.get('*', [ cookieParser(), SessionClient ], function(req, res) {
 	var libs = /\/libs\//.exec(req.pathname);
 	if(!libs) {
-		var ip = req.headers['x-forwarded-for'] || 
-		     req.connection.remoteAddress || 
-		     req.socket.remoteAddress ||
-		     req.connection.socket.remoteAddress;
-		console.log(ip);
+
 		var db = conn.connect();
-	  	db.select('sys_sessions', { session_id: req.session }, function(err, row, field){
-	  		if((row || []).length == 0) db.insert('sys_sessions', { session_id: req.session, email: null, expire_at: 0, created_at: req.timestamp });
-	  		var name = ((err || {}).name != undefined ? err.name : "");
-	  		var message = ((err || {}).message != undefined ? '('+err.statusCode+') '+err.code+' - '+err.message : "");
+		var where = { access_id: req.access, session_id: req.session, today: req.timestamp };
+	  	db.query('SELECT * FROM sys_sessions WHERE (session_id = :session_id OR access_id = :access_id)', where, function(err, row, field){
+	  		if((row || []).length == 0) {
+	  			db.insert('sys_sessions', { access_id: 'UNKNOW', session_id: req.session, email: null, expire_at: 0, created_at: req.timestamp });
+	  		}
 	  		res.render('index', { 
 	  			_LANG: language, 
 	  			_HOST: config.ip+':'+config.port, 
-	  			_SESSION : { ID: req.session, NAME: name, MESSAGE: message }
+	  			_SESSION : { 
+	  				ID: (!req.access) ? req.session : undefined , 
+	  				NAME: (err || {}).name != undefined ? err.name : "", 
+	  				MESSAGE: (err || {}).message != undefined ? '('+err.statusCode+') '+err.code+' - '+err.message : "" 
+	  			}
 	  		});
 		});
 	} else {
