202e990c15c7c907f668fb6a9b80565c05724654
Kananek Thongkam <info.dvgamer@gmail.com>
2016-01-26 09:42:38 +0700

    update event not found session in db

--- api/sign-access.js
+++ api/sign-access.js
@@ -2,10 +2,9 @@ var conn = require('../library/db');
 
 module.exports = function(req, res, data){
 	var db = conn.connect();
-	db.update('sys_sessions', { access_id: req.access }, { session_id: req.session }).then(function(){
-	  	db.selectOne('sys_sessions', { email: data.email }, function(err, row, field){ //LEVEL 3
-	  		if(row) res.success({ access: true }); else	res.success({ access: false });
+	db.update('sys_sessions', { access_id: req.access }, { session_id: req.session }, function(err, accessRow, field){
+	  	db.selectOne('sys_sessions', { email: data.email }, function(err, emailRow, field){ //LEVEL 3
+			res.success({ access: emailRow ? true : false, found: accessRow.affectedRows > 1 });
 		});
 	});
-
 }
