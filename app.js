dacaa302702938526b7c9358c1ba33cdcbf82870
Kananek Thongkam <info.dvgamer@gmail.com>
2016-02-10 09:57:27 +0700

    change title cmd

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var systems = require('./systems');
 var config = systems.config;
 //var db = monk(config.mongo_db.doamin+':'+config.mongo_db.port+'/'+config.mongo_db.dbname, config.mongo_db.access);
 
-process.title = "touno-k.noip.me:"+config.port;
+process.title = "pgm.ns.co.th:"+config.port;
 
 systems.Run();
