6be3d2063df670f53e8fee3f3be3c928ba53d3e3
Kananek T <info.dvgamer@gmail.com>
2016-03-09 16:05:26 +0700

    update forgot

--- client/templates/sign/sign-forgot.js
+++ client/templates/sign/sign-forgot.js
@@ -1,3 +1,12 @@
 Template.signForgot.onRendered(function() {
   $(window).resize();
 });
+
+Template.signForgot.events({
+  'click .forgot-back.button': function(){
+  	Router.go('home');
+  },
+  'click .forgot-in.button': function(){
+  	Router.go('home');
+  }
+});
