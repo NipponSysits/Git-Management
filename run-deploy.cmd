1d0b7bdc6521690f7aac6b4a1b7b21a0c58dcab4
Kananek T <info.dvgamer@gmail.com>
2016-03-31 11:30:24 +0700

    update build files

--- run-deploy.cmd
+++ run-deploy.cmd
@@ -1,5 +1,6 @@
 @echo off
-meteor npm install
-meteor build ./.script/ --mobile-settings settings.json --architecture=os.linux.x86_64
-
+SET ON_DATE=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%
+SET /P version=<.meteor\deploy\build-versions
+SET /A version=%version%+1 > .meteor\deploy\build-versions
+meteor build .meteor/deploy/builds_%ON_DATE%_version-%version% --mobile-settings settings.json --server-only --architecture=os.linux.x86_64
 @echo on
