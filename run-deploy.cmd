b5d5987942f7e37b01f720ce33169d2dde58865a
Kananek T <info.dvgamer@gmail.com>
2016-05-24 13:29:54 +0700

    update package

--- run-deploy.cmd
+++ run-deploy.cmd
@@ -1,12 +1,12 @@
 @echo off
-SET ON_DATE=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%
-SET /P version=<.meteor\deploy\build-versions
-SET /A version=%version%+1
-SET BUNDLE=builds_%ON_DATE%_version-%version%
-SET DOCKER=pgm.ns.co.th
-ECHO %version% > .meteor\deploy\build-versions
-TITLE meteor - versions %version%
-meteor build .meteor/ --mobile-settings settings.json --server-only --architecture=os.linux.x86_64
+rem SET ON_DATE=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%
+rem SET /P version=<.meteor\deploy\build-versions
+rem SET /A version=%version%+1
+rem SET BUNDLE=builds_%ON_DATE%_version-%version%
+rem SET DOCKER=pgm.ns.co.th
+rem ECHO %version% > .meteor\deploy\build-versions
+rem TITLE meteor - versions %version%
+meteor build /data/app-bundle/ --directory --architecture=os.linux.x86_64
 :: RENAME debuger-Management.tar.gz %BUNDLE%.tar.gz
 :: ECHO 
 :: winscp /command ^
