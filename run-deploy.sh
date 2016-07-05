a076723534387062789488ca2bc581c25699e3aa
Kananek T <info.dvgamer@gmail.com>
2016-07-05 20:22:55 +0700

    update meteor version

--- run-deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git pull origin master
-meteor build /data/app-bundle/ --directory --architecture=os.linux.x86_64
-cd /app
-docker-compose restart debugerr
-
-git clone -b latest https://git@github.com/username/myproject.git
